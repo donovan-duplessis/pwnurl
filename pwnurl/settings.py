738d25f36432d7712b93eeb7bf2ae71a7df20ac3
Donovan du Plessis <donovan@binarytrooper.com>
2014-04-17 15:32:47 +0200

    Minor updates

--- pwnurl/settings.py
+++ pwnurl/settings.py
@@ -1,41 +1,52 @@
 # -*- coding: utf-8 -*-
+
 import os
 
 
 class Config(object):
-    SECRET_KEY = 'shhhh'
+
+    """ Base Configuration """
+
+    SECRET_KEY = 't*wrw^(47cpu&n=a4)4$v4*vk2gfy-'
     PORT = 5000
-    APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LEVEL = 13
-    DEBUG_TB_ENABLED = False  # Disable Debug toolbar
+    DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
-    CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
+    CACHE_TYPE = 'simple'
 
 
 class ProdConfig(Config):
-    '''Production configuration.'''
+
+    """ Production Configuration """
+
     ENV = 'prod'
-    DEBUG = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:Cr0nus@localhost/pwnurl'
-    DEBUG_TB_ENABLED = False  # Disable Debug toolbar
+    DB_NAME = 'prod.db'
+    DEBUG = False
+    DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
+    DEBUG_TB_ENABLED = False
 
 
 class DevConfig(Config):
-    '''Development configuration.'''
+
+    """ Development Configuration """
+
     ENV = 'dev'
     DEBUG = True
-    DB_NAME = "dev.db"
-    # Put the db file in project root
+    DB_NAME = 'dev.db'
     DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
-    SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(DB_PATH)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
     DEBUG_TB_ENABLED = True
-    ASSETS_DEBUG = True  # Don't bundle/minify static assets
-    CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
+    ASSETS_DEBUG = True
+    CACHE_TYPE = 'simple'
 
 
 class TestConfig(Config):
+
+    """ Testing Configuration """
+
     TESTING = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
--- pwnurl/utils.py
+++ pwnurl/utils.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-'''Helper utilities and decorators.'''
+
+""" Helper utilities and decorators """
+
 from flask import flash
 
-def flash_errors(form, category="warning"):
-    '''Flash all errors for a form.'''
-    for field, errors in form.errors.items():
+
+def flash_errors(form, category='warning'):
+
+    """ Flash all form error messages """
+
+    for (field, errors) in form.errors.items():
         for error in errors:
-            flash("{0} - {1}"
-                    .format(getattr(form, field).label.text, error), category)
+            flash('{0} - {1}'.format(getattr(form, field).label.text, error),
+                  category)
